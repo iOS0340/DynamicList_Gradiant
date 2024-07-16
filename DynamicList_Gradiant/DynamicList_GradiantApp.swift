6a73c731f49b91c3b6607bde4d0a807ce37639ec
Bhavesh Patel <bhavesh.patel0340@gmail.com>
2024-07-16 10:35:48 +0530

    Initial Commit

---
+++ DynamicList_Gradiant/DynamicList_GradiantApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicList_GradiantApp.swift
+//  DynamicList_Gradiant
+//
+//  Created by Bhavesh Patel on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DynamicList_GradiantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
