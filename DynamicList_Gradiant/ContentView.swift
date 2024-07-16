9449dbcce3f6df127d5b93ad2f0643986e86297e
Bhavesh Patel <bhavesh.patel0340@gmail.com>
2024-07-16 11:47:24 +0530

    Create List with Gradiant and give visual effect and scale effect.

--- DynamicList_Gradiant/ContentView.swift
+++ DynamicList_Gradiant/ContentView.swift
@@ -9,16 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.vertical){
+            VStack {
+                ForEach(0..<25) { index in
+                    RoundedRectangle(cornerRadius: 12, style: .continuous)
+                        .fill(randomColor().gradient)
+                        .frame(height: 100)
+                        .visualEffect { content, proxy in
+                                                    let frame = proxy.frame(in: .scrollView(axis: .vertical))
+                                                    let distance = min(0, frame.minY)
+//                            print("Distance \(distance) And Frame \(frame) And index \(index)")
+                            return content
+//                                .hueRotation(.degrees(frame.origin.y / 5))
+                                .scaleEffect(1 + distance / (UIScreen.current?.bounds.height)!)
+                                .offset(y: -distance / 1.35)
+                                .brightness(-distance / (UIScreen.current?.bounds.height)!)
+                                .blur(radius: -distance / 50)
+                                                }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+func randomColor() -> Color{
+    return Color(red: getRandomValue(), green: getRandomValue(), blue: getRandomValue());
+}
+
+func getRandomValue() -> Double{
+    return Double.random(in: 0...1);
 }
